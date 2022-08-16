293e6acd8e12bff1b2bd32fec3b7ff9883adcfc6
v69ar73aqvc <54872398+v69ar73aqvc@users.noreply.github.com>
2022-08-16 14:31:29 -0700

    Update VisualStudioSupport.cs

--- src/CrossLaunch.Ubiquitous/VisualStudioSupport.cs
+++ src/CrossLaunch.Ubiquitous/VisualStudioSupport.cs
@@ -40,7 +40,16 @@ public class VisualStudioProjectLoader : IProjectLoader
     public Task<ProjectLoadResult> TryLoadAsync(BaseProjectModel project, CLConfiguration configuration)
     {
         if (!VSSolutionFile.TryGetMinimumVisualStudio(project, out string? framework)) return Task.FromResult(ProjectLoadResult.BadFrameworkId(project.Framework));
-        // TODO parse out solution into project references
+        VSSolutionFile solutionFile;
+        try
+        {
+            using var stream = File.OpenText(project.FullPath);
+            solutionFile = VSSolutionFile.Load(stream);
+        }
+        catch (InvalidDataException)
+        {
+            return Task.FromResult(ProjectLoadResult.InvalidFile);
+        }
         var remediations = new List<ProjectLoadFailRemediation>();
         if (configuration.TryGetFlag("visualstudio.rider.enable", out bool riderEnabled))
         {
@@ -76,11 +85,13 @@ public class VSSolutionFile
 {
     public readonly string MinimumVisualStudioVersion;
     public readonly string VisualStudioVersion;
+    public IReadOnlyList<ProjectDefinition> ProjectDefinitions;
 
-    public VSSolutionFile(string minimumVisualStudioVersion, string visualStudioVersion)
+    public VSSolutionFile(string minimumVisualStudioVersion, string visualStudioVersion, IReadOnlyList<ProjectDefinition> projectDefinitions)
     {
         MinimumVisualStudioVersion = minimumVisualStudioVersion;
         VisualStudioVersion = visualStudioVersion;
+        ProjectDefinitions = projectDefinitions;
     }
 
     public static VSSolutionFile Load(TextReader reader)
@@ -101,13 +112,71 @@ public class VSSolutionFile
             if (TryGetKeyValue(l, "MinimumVisualStudioVersion", out var tmpMinimumVisualStudioVersion)) minimumVisualStudioVersion = tmpMinimumVisualStudioVersion;
             if (visualStudioVersion.Length != 0 && minimumVisualStudioVersion.Length != 0) break;
         }
-        if (visualStudioVersion.Length != 0 && minimumVisualStudioVersion.Length != 0)
+        if (visualStudioVersion.Length == 0 || minimumVisualStudioVersion.Length == 0) throw new InvalidDataException("Missing version info");
+        ParseState state = new(new List<ProjectDefinition>());
+        ProcessSection(ReadOnlySpan<char>.Empty, ref state, reader);
+        return new VSSolutionFile(new string(minimumVisualStudioVersion), new string(visualStudioVersion), state.ProjectDefinitions);
+    }
+
+    private static void ProcessSection(ReadOnlySpan<char> section, ref ParseState state, TextReader reader)
+    {
+        string? line;
+        while ((line = reader.ReadLine()) != null)
         {
-            var sb = new StringBuilder();
-            sb.Append(minimumVisualStudioVersion).Append('/').Append(visualStudioVersion);
-            return new VSSolutionFile(new string(minimumVisualStudioVersion), new string(visualStudioVersion));
+            ReadOnlySpan<char> l = line.TrimStart();
+            if (section.Length != 0 && l.StartsWith("End") && l[3..].TrimEnd().SequenceEqual(section))
+            {
+                return;
+            }
+            ReadOnlySpan<char> key;
+            ReadOnlySpan<char> name = ReadOnlySpan<char>.Empty;
+            ReadOnlySpan<char> value;
+            if (TryGetKeyValue(l, out var k1, out var v1))
+            {
+                key = k1;
+                value = v1;
+                if (TryGetNamedKey(key, out var k2, out var n1))
+                {
+                    key = k2;
+                    name = n1;
+                }
+            }
+            else
+            {
+                key = l;
+                value = ReadOnlySpan<char>.Empty;
+            }
+            if (key.SequenceEqual("Project"))
+            {
+                ProcessSection("Project", ref state, reader);
+                var nameEntries = ParseEntries(name);
+                var valueEntries = ParseEntries(value);
+                if (nameEntries.Count != 1) throw new InvalidDataException("Unexpected number of entries for Project element kind");
+                if (!Guid.TryParseExact(nameEntries[0], "B", out var nameGuid)) throw new InvalidDataException("Invalid GUID for Project element kind");
+                if (valueEntries.Count != 3) throw new InvalidDataException("Unexpected number of entries for Project element data");
+                if (!Guid.TryParseExact(valueEntries[2], "B", out var projectGuid)) throw new InvalidDataException("Invalid GUID for Project");
+                state.ProjectDefinitions.Add(new ProjectDefinition(nameGuid, valueEntries[0], valueEntries[1], projectGuid));
+            }
+            else if (key.SequenceEqual("Global"))
+            {
+                ProcessSection("Global", ref state, reader);
+            }
+            else if (key.SequenceEqual("GlobalSection"))
+            {
+                ProcessSection("GlobalSection", ref state, reader);
+            }
         }
-        throw new InvalidDataException("Missing version info");
+        if (section.Length != 0) throw new InvalidDataException("Unexpected EOF");
+    }
+
+    private struct ParseState
+    {
+        public List<ProjectDefinition> ProjectDefinitions;
+
+        public ParseState(List<ProjectDefinition> projectDefinitions)
+        {
+            ProjectDefinitions = projectDefinitions;
+        }
     }
 
     private static bool TryGetKeyValue(ReadOnlySpan<char> source, ReadOnlySpan<char> pattern, out ReadOnlySpan<char> result)
@@ -123,6 +192,34 @@ public class VSSolutionFile
             }
         }
         result = ReadOnlySpan<char>.Empty;
+        return false;
+    }
+
+    private static bool TryGetKeyValue(ReadOnlySpan<char> source, out ReadOnlySpan<char> key, out ReadOnlySpan<char> value)
+    {
+        int index = source.IndexOf('=');
+        if (index != -1)
+        {
+            key = source[..index].Trim();
+            value = source[(index + 1)..].Trim();
+            return true;
+        }
+        key = ReadOnlySpan<char>.Empty;
+        value = ReadOnlySpan<char>.Empty;
+        return false;
+    }
+
+    private static bool TryGetNamedKey(ReadOnlySpan<char> key, out ReadOnlySpan<char> keyType, out ReadOnlySpan<char> keyName)
+    {
+        int index = key.IndexOf('(');
+        if (index != -1 && key[^1] == ')')
+        {
+            keyType = key[..index];
+            keyName = key[(index + 1)..^1];
+            return true;
+        }
+        keyType = ReadOnlySpan<char>.Empty;
+        keyName = ReadOnlySpan<char>.Empty;
         return false;
     }
 
@@ -143,4 +240,25 @@ public class VSSolutionFile
         result = null;
         return false;
     }
+
+    private static List<string> ParseEntries(ReadOnlySpan<char> buf)
+    {
+        var res = new List<string>();
+        buf = buf.Trim();
+        while (!buf.IsEmpty)
+        {
+            if (buf[0] != '"') throw new InvalidDataException("Expected '\"'");
+            buf = buf[1..];
+            int index2 = buf.IndexOf('"');
+            if (index2 == -1) throw new InvalidDataException("Expected '\"'");
+            ReadOnlySpan<char> sub = buf[..index2];
+            buf = buf[(index2 + 1)..].TrimStart();
+            if (buf.StartsWith(",")) buf = buf[1..].TrimStart();
+            else if (!buf.IsEmpty) throw new InvalidDataException("Expected ''");
+            res.Add(new string(sub));
+        }
+        return res;
+    }
+
+    public record ProjectDefinition(Guid Kind, string Directory, string File, Guid Guid);
 }
--- src/CrossLaunch/ProjectLoadResult.cs
+++ src/CrossLaunch/ProjectLoadResult.cs
@@ -4,6 +4,8 @@ public record ProjectLoadResult(bool Success, ProjectLoadFailInfo? FailInfo)
 {
     public static readonly ProjectLoadResult Successful = new(true, null);
 
+    public static readonly ProjectLoadResult InvalidFile = new(false, new ProjectLoadFailInfo("Invalid Project File(s)", "Project file(s) could not be read.", Array.Empty<ProjectLoadFailRemediation>()));
+
     public static ProjectLoadResult Failure(ProjectLoadFailInfo? failInfo = null) => new(false, failInfo);
 
     public static ProjectLoadResult Failure(string title, string errorMessage, params ProjectLoadFailRemediation[] remediations) => new(false, new ProjectLoadFailInfo(title, errorMessage, remediations));
